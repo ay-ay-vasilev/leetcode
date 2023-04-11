431a08effbad8a3f479f9f2be5d1d6cd8f608c25
ay-ay-vasilev <ay.ay.vasilev@gmail.com>
2023-04-11 10:19:09 +0700

    3. Longest Palindromic Substring

---
+++ 5_Longest_Palindromic_Substring.cpp
@@ -0,0 +1,23 @@
+//#include <string>
+//
+//using namespace std;
+//
+//string longestPalindrome(string s) {
+//	int i = 0, l = 0, r = 0, lhold = l, rhold = r, n = s.size();
+//
+//	while (i < n)
+//	{
+//		l = i, r = i;
+//		while (r < (n - 1) && s[r] == s[r + 1]) r++;
+//		i = r + 1;
+//
+//		while (l > 0 && r < (n - 1) && s[l - 1] == s[r + 1]) l--, r++;
+//
+//		if (r - l > rhold - lhold)
+//		{
+//			lhold = l;
+//			rhold = r;
+//		}
+//	}
+//	return s.substr(lhold, rhold - lhold + 1);
+//}
--- LeetCode.cpp
+++ LeetCode.cpp
@@ -1,8 +1,42 @@
 #include <iostream> 
 #include <string>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
+string longestPalindrome(string s) {
+	int n = s.length();
+	vector<vector<int>> matrix(n, vector<int>(n, 0));
+	for (int i = 0; i < n; i++)
+	{
+		matrix[i][i] = 1;
+	}
+	int longestSubstrLength = 1;
+	string longestSubstr(1, s[0]);
+	for (int i = n - 1; i >= 0; i--)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (s[i] == s[j])
+			{
+				if (matrix[i + 1][j - 1] != 0 || j - i == 1)
+				{
+					matrix[i][j] = 1;
+					if (longestSubstrLength < j - i + 1)
+					{
+						longestSubstrLength = j - i + 1;
+						longestSubstr = s.substr(i, longestSubstrLength);
+					}
+				}
+			}
+		}
+	}
+	return longestSubstr;
+}
+
 int main() {
+	const auto result = longestPalindrome("aacabdkacaa");
+	cout << result << "\n";
 	return 0;
 }
