66ebeced2d6e8b1ce3540c769d754d056db7f55a
ay-ay-vasilev <ay.ay.vasilev@gmail.com>
2023-05-02 21:06:05 +0700

    42. House Robber

---
+++ 198_House_Robber.cpp
@@ -0,0 +1,20 @@
+#include "stdc++.h"
+
+using namespace std;
+
+class Solution {
+public:
+	int rob(vector<int>& nums) {
+		int n = nums.size();
+
+		if (n == 1) return nums[0];
+
+		vector<int> dp(nums);
+		dp[1] = max(nums[1], nums[0]);
+
+		for (int i = 2; i < n; ++i)
+			dp[i] = max(nums[i] + dp[i - 2], dp[i - 1]);
+
+		return dp[n - 1];
+	}
+};
